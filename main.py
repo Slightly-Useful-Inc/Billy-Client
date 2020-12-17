5487428df31e2a3c83cd48f5c885d12e749500df
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-12-17 00:27:18 -0500

    Added a lot, still need to optimizw

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-import requests
+import speech_recognition as sr
+import webbrowser
+import time
+import os
+import playsound
+from gtts import gTTS
+import random
 
-def checkServer():
-    try:
-        online = requests.get("http://localhost/")
-        if str(online.json()) == "{'status': 200}":
-            return True
-        elif str(online.json()) == "{'status': 503":
-            return False
-    except Exception:
-        return False
+r = sr.Recognizer()
+
+def get_audio(ask = False):
+    with sr.Microphone() as source:
+        if ask:
+            billyTalk(ask)
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except  sr.UnknownValueError:
+            billyTalk("Come Again")
+        except sr.RequestError:
+            billyTalk("Network Error")
+        return voice_data
 
 
-if checkServer():
-    print("Servers Are Up")
-else:
-    print(checkServer())
+def billyTalk(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-'+ str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        billyTalk("Billy")
+    if 'search' in voice_data:
+        search = get_audio('What do you want to find?')
+        url = 'https://google.com/search?q='+search
+        webbrowser.get().open(url)
+        billyTalk("This is what I found for " + search)
+
+
+time.sleep(1)
+billyTalk("How can I help you!")
+while 1:
+    voice_data = get_audio()
+    respond(voice_data)
--- web/search.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-
-
-
-def find(search = None):
-    if search is None:
-        return TypeError
-    else:
-        return wikipedia.search(search)
