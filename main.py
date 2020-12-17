fb92e7ab064a3181358cba954fcb7612698a6058
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-12-17 12:23:54 -0500

    Added Server Auth

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import sys, time
 import voice.reqRes
+import api.user, api.server
 
-req = voice.reqRes.reqRes()
 
-time.sleep(1)
-req.billyTalk("How can I help you!")
-while 1:
-    req.voice_data = req.get_audio()
-    req.respond(req.voice_data)
+checkServer = api.server.severStatus()
+
+if checkServer:
+    username = input("Username: ")
+    password = input("Password: ")
+    login = api.user.userAuth(username, password)
+    if login:
+        req = voice.reqRes.reqRes()
+
+        time.sleep(1)
+        req.billyTalk("How can I help you!")
+        while 1:
+            req.voice_data = req.get_audio()
+            req.respond(req.voice_data)
+    else:
+        print("Incorrect Username or Password")
+
+else:
+    print("Server Error, check your internet connection")
